7d304425829f19c0bfb099ca88b9e8d806b19fdc
SG <sushanta1996@gmail.com>
2022-03-05 15:24:46 +1100

    ala-search adding endpoint variable, updating comments

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,120 +6,141 @@ import { PageEvent } from '@angular/material/paginator';
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  // search term and result objets
-  searchTerm: string = '';
-  searchResults: any = {results: []};
-  errorMessage: string = '';
-  statusMessage: string = '';
+	// search endpoint
+	searchEndpoint: string = 'https://bie.ala.org.au/ws/search.json';
 
-  // search filters and limits
-  resultsPerpage = 10;
+	// search term and result objets
+	searchTerm: string = '';
+	searchResults: any = {results: []};
+	errorMessage: string = '';
+	statusMessage: string = '';
 
-  // state variable
-  isLoading: boolean = false;
 
-  // timeout object
-  timeout: any;
+	// search filters and limits
+	resultsPerpage = 10;
 
-  constructor() { }
+	// state variable
+	isLoading: boolean = false;
 
-  ngOnInit(): void {
-  }
+	// timeout object
+	timeout: any;
 
-  handleSearchInput(event: any){
-	this.searchTerm = event.target.value || '';
-	this.handleSearch();
+	constructor() { }
 
-  }
+	/**
+	 * Handle input event from search bar
+	 * @param event 
+	 */
+		handleSearchInput(event: any){
+			this.searchTerm = event.target.value || '';
+			this.handleSearch();
 
-  handleSearch(){
-	// clear the delay timeout if it's already active. 
-    if(this.timeout !== undefined){
-    	clearTimeout(this.timeout);
-    }	
-	// execute after a 500ms deplay to allow  to prevents requets from being sent while user is still tying
-	this.timeout = setTimeout(() => {
-		if(this.searchTerm.length > 0){
-			// set loading state	
-			this.isLoading = true;
-			const request = new Request(`https://bie.ala.org.au/ws/search.json?q=${this.searchTerm}&fq=&dir=desc&sortField=score&rows=${this.resultsPerpage}`, {method: 'GET', mode:'cors'});
-			// fetch the request
-			fetch(request).then(async response => {
-				// if the http request is successful and the response is a 200/202, handle the data
-				if(response.ok){
-					const data = await response.json();
-					this.searchResults = data['searchResults'] || {};
-					this.errorMessage = '';
-				} else{
+	}
+
+    // Handle the main search operation itself
+	handleSearch(){
+		// clear the delay timeout if it's active from previous call 
+		if(this.timeout !== undefined){
+			clearTimeout(this.timeout);
+		}	
+		// execute after a 500ms deplay to prevent requets from being sent while user is still tying
+		this.timeout = setTimeout(() => {
+			if(this.searchTerm.length > 0){
+				// set loading state	
+				this.isLoading = true;
+				const request = new Request(`${this.searchEndpoint}?q=${this.searchTerm}&fq=&dir=desc&sortField=score&rows=${this.resultsPerpage}`, {method: 'GET', mode:'cors'});
+				// fetch the request
+				fetch(request).then(async response => {
+					// if the http request is successful and the response is a 200/202, handle the data
+					if(response.ok){
+						const data = await response.json();
+						this.searchResults = data['searchResults'] || {};
+						this.errorMessage = '';
+					} else{
+						this.errorMessage = `Error returning resultsf for '${this.searchTerm}' Please try again later.`;
+						this.searchResults = {};
+					}
+					// catch request failures
+				}).catch(e => {
+					console.error(e);
 					this.errorMessage = `Error returning resultsf for '${this.searchTerm}' Please try again later.`;
 					this.searchResults = {};
+					// handle on success or fail.
+				}).finally(() => {
+					// reset loading state
+					this.isLoading = false;
+				})
+			}else{
+				this.searchResults = {};
+			}
+
+		}, 500);
+	}
+
+	/**
+	 * clear search tem and results
+	 */
+	clearSearch(){
+		this.searchTerm = '';
+		this.searchResults = {};
+	}
+
+	/**
+	 * open image url in a new tab
+	 * @param url 
+	 */
+	openImage(url: string){
+		window.open(url, '_blank')
+	}
+
+	/**
+	 * handles partial pagination i.e. items per page since full offset search is not avilable on the API. 
+	 * @param event 
+	 */
+	handlePaginate(event: PageEvent){
+		this.resultsPerpage = event.pageSize;
+		this.handleSearch();
+	}
+
+    // fetch 100 records (if required) and export to csv
+	handleDownload(){
+		if(this.resultsPerpage === 100 && this.searchResults.results.length === 100){
+			this.exportCSV(this.searchResults.results);
+		} else{
+			const request = new Request(`${this.searchEndpoint}?q=${this.searchTerm}&fq=&dir=desc&sortField=score&rows=100`, {method: 'GET', mode:'cors'});
+			fetch(request).then(async response => {
+				// if the request is successful and the response is a 200/202, handle the data
+				if(response.ok){
+					const data = await response.json();
+					const results = data['searchResults'].results;
+					this.exportCSV(results);
+					this.errorMessage = '';
+				} else{
+					this.errorMessage = 'Error returning results. Please try again later.';
 				}
 				// catch request failures
 			}).catch(e => {
-				console.error(e);
-				this.errorMessage = `Error returning resultsf for '${this.searchTerm}' Please try again later.`;
-				this.searchResults = {};
-				// handle on success or fail.
-			}).finally(() => {
-				// reset loading state
-				this.isLoading = false;
-			})
-		}else{
-			this.searchResults = {};
+				console.log(e);
+				this.errorMessage = 'Error returning results. Please try again later.';
+			});
 		}
-	
-	}, 500);
-  }
-
-  clearSearch(){
-	  this.searchTerm = '';
-	  this.searchResults = {};
-  }
-
-  openImage(url: string){
-	  	window.open(url, '_blank')
-  }
-
-  handlePaginate(event: PageEvent){
-	  this.resultsPerpage = event.pageSize;
-	  this.handleSearch();
-  }
-
-    // fetch 100 records (if required) and export to csv
-  handleDownload(){
-	if(this.resultsPerpage === 100 && this.searchResults.results.length === 100){
-		this.exportCSV(this.searchResults.results);
-	} else{
-		const request = new Request(`https://bie.ala.org.au/ws/search.json?q=${this.searchTerm}&fq=&dir=desc&sortField=score&rows=100`, {method: 'GET', mode:'cors'});
-		fetch(request).then(async response => {
-			// if the http request is successful and the response is a 200/202, handle the data
-			if(response.ok){
-				const data = await response.json();
-				const results = data['searchResults'].results;
-				this.exportCSV(results);
-				this.errorMessage = '';
-			} else{
-				this.errorMessage = 'Error returning results. Please try again later.'
-			}
-			// catch request failures
-		}).catch(e => {
-			console.log(e);
-			this.errorMessage = 'Error returning results. Please try again later.'
-			// handle on success or fail.
-		});
 	}
-  }
   
 
-  // based on a TS implementation by Jorge Garcia of a generic csv export function 
-  // https://www.clearpeople.com/blog/export-data-to-csv-with-typescript-without-format-issues
-  exportCSV(results: any[]){
+	/**
+	 * based on a TS implementation by Jorge Garcia of a generic csv export function 
+	 * Abaliable at: https://www.clearpeople.com/blog/export-data-to-csv-with-typescript-without-format-issues
+	 * @param results 
+	 */
+    exportCSV(results: any[]){
+		// set file name to the searvh tem. can be updated by the user durring the browsers doanload process
 		const fileName = this.searchTerm;
+		// requied attributes attributes
 		const exportHeaders  = ['id','guid','kingdom','kingdomGuid','scientificName','author','imageUrl'];
 		const exportRows:any[] = [];
-		// generate a new rows object with only the required attrinutes
+		// generate a new rows object with only the required attributes
 		results.forEach(r => {
 			exportRows.push({id:r.id, guid: r.guid, kingdom:r.kingdom, kingdomGuid:r.kingdomGuid, scientificName:r.scientificName, author:r.author,imageUrl:r.imageUrl});
 		});
@@ -133,7 +154,7 @@ export class SearchComponent implements OnInit {
 			exportHeaders.join(separator) +
 			'\n' +
 			exportRows.map(row => {
-				return keys.map(k => {
+			return keys.map(k => {
 					let cell = row[k] === null || row[k] === undefined ? '' : row[k];
 
 					cell = cell instanceof Date 
@@ -145,7 +166,7 @@ export class SearchComponent implements OnInit {
 					}
 					return cell;
 				}).join(separator);
-			}).join('\n');
+		}).join('\n');
 
 		const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
 		// @ts-ignore
